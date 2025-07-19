5ee6b8718948fcc6b1406f992f6a874b355de734
anjalijoshi-gif <81000450+anjalijoshi-gif@users.noreply.github.com>
2025-07-19 14:10:03 +0400

    Add files via upload

---
+++ Day1Assignment.js
@@ -0,0 +1,28 @@
+//Assignment Day 1 : 
+//a) Setup your system with installing node.js and VScode.
+//b) Create a simple project by selecting an existing or by creating a new folder.
+//c) Write few print statements with the help of console.log() 
+//d) Declare few variables with let and const keywords
+
+console.log("First Assignment");
+console.log("Hello Javascript");
+console.log("UK is nice country");
+console.log("hi");
+
+let number = 32;
+let names = 'Anjali, Joshi , UAE';
+let c= 'a';
+let learn = "nishta";
+
+const a= 33;
+const a1 = 333;
+
+let b =2;
+let b1 = 3;
+
+
+console.log(number);
+console.log(names);
+console.log(c);
+console.log(learn);
+
---
+++ Day2Assignment.js
@@ -0,0 +1,44 @@
+/*Assignment Day 2: 
+a) Study primitive and non primitive data types by refering to my recording and can explore it on google too.
+b) write a set of code with below sample by adding more types of arithmetic operator like(minus(-),multiply(*),divide(/),remainder(%) and replace it by plus(+) sign in below example.
+Example:
+let total = 10;
+total +=5; //total = total + 5*/
+
+
+let num1 = 100;
+let num2 = 200;
+console.log(num1-num2);
+console.log(num1+num2);
+console.log(num1*num2);
+console.log(num1%num2);
+console.log(num1/num2);
+console.log(num1**num2);
+console.log(++num1);
+console.log(num1 --);
+console.log(num1 --);
+
+
+
+console.log()
+//primitive data types
+let x = 250;
+let y = 22.5;
+console.log(`Addition of x and y is ${x+y}`);
+
+let a = 100;
+let b = 300;
+console.log(`Addition of a and b is    ${a+b}`);
+console.log(`Subtraction of a and b is ${a-b}`);
+console.log(`multiplication of a and b is  ${a*b}`);
+console.log(`Divison of a and b is ${a/b}`);
+console.log(` equal to a and b is ${a=b}`);
+
+
+let person =  {
+    names :"Anjali",
+    age : "30",
+    place: "uae" ,
+}
+
+console.log(person);
